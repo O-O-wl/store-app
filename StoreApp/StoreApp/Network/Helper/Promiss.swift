bf30e601c0fb320339f6926e13a7b3383f4bcd7b
부엉이 <o.o.wl.soft@gmail.com>
2019-12-24 05:17:57 +0900

    feat: 비동기를 동기방식으로 구현하기위한 Promiss 타입 추가

--- StoreApp/StoreApp/Network/Helper/Promiss.swift
+++ StoreApp/StoreApp/Network/Helper/Promiss.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+
+class Promiss<T> {
+    
+    // MARK: - Properties
+    
+    var value: T? {
+        didSet {
+            let task = DispatchWorkItem(block: { self.handler?(self.value) })
+            workSpace(task)
+        }
+    }
+    
+    private var workSpace = DispatchQueue.global().async(execute:)
+    
+    private var handler: ((T?) -> Void)? {
+        didSet {
+            let task = DispatchWorkItem(block: { self.handler?(self.value) })
+            workSpace(task)
+        }
+    }
+    // MARK: - Methods
+    
+    @discardableResult
+    func workSpace(_ type: ProcessingThread) -> Self {
+        switch type {
+        case .main:
+            self.workSpace = DispatchQueue.main.async(execute:)
+        case .global:
+            self.workSpace = DispatchQueue.global().async(execute:)
+        }
+        
+        return self
+    }
+    
+    func handle(_ handler: ((T?) -> Void)?) {
+        self.handler = handler
+    }
+    
+    // MARK: - ProcessingTread
+    
+    enum ProcessingThread {
+        case main
+        case global
+    }
+}
+
