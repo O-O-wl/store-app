51a800b42e8602a946a932b5c39a07895a54496b
부엉이 <o.o.wl.soft@gmail.com>
2019-12-24 05:18:26 +0900

    feat: BaminChanAPI 타입 추가

--- StoreApp/StoreApp/Network/EndPoint/BaminChanAPI.swift
+++ StoreApp/StoreApp/Network/EndPoint/BaminChanAPI.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+
+enum BaminChanAPI {
+    case main
+    case soup
+    case side
+}
+
+extension BaminChanAPI: EndPoint {
+    
+    var baseURL: URL {
+        return URL(string:"https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/baminchan")!
+    }
+    
+    var path: String {
+        switch self {
+        case .main: return "main"
+        case .soup: return "soup"
+        case .side: return "side"
+        }
+    }
+    
+    var method: HTTP.Method {
+        return .get
+    }
+    
+    var task: HTTP.Task {
+        return .request
+    }
+    
+    var headers: HTTP.Headers? {
+        return nil
+    }
+    
+    var sampleData: Data? {
+        return nil
+    }
+    
+    func asURLRequest() -> URLRequest {
+        return URLRequest(baseURL, path: path, method: method, headers: headers)
+    }
+}
